c33f04d72b79042fa0a68a0e83d77697435700ec
nathaniel-kk <1286184117@qq.com>
2020-11-13 17:48:28 +0800

    init

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::prefix('auth')->namespace('Auth')->group(function () {
+    Route::post('login', 'AuthController@login'); //普通登陆
+    Route::post('adminlogin', 'AuthController@adminLogin'); //超级管理员登陆
+    Route::post('logout', 'AuthController@logout'); //退出登陆
+    Route::post('refresh', 'AuthController@refresh'); //刷新token
+    Route::post('register', 'AuthController@registered'); //注册接口
+});
